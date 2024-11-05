c916736c243f2c9cde9770831f21b6201f4bad44
Tejas Raghuvanshi <tejasraghuvanshi23@gmail.com>
2024-11-05 04:58:46 -0500

    fixes

--- Assets/Scripts/Control-Movement/CameraControl.cs
+++ Assets/Scripts/Control-Movement/CameraControl.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    float speedV = 2.0f;
+    private PlayerInputActions _inputActions;
+    private float _udInput;
+    // private float _ewInput;
+    private float speedV = 2.0f;
+    
     float minPitch = -45.0f;
     float maxPitch = 89.99f;
     private float pitch = 25f;
     Vector3 startVector = new Vector3(0, 0, -12f);
 
+    void Awake()
+    {
+        _inputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        _inputActions.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        _inputActions.Gameplay.Disable();
+    }
+    
     private void Start()
     {
         transform.localPosition = startVector;
@@ -17,7 +36,12 @@ public class CameraControl : MonoBehaviour
     }
     void Update()
     {
-        pitch += speedV * (Input.GetAxis("RJoy Y") - Input.GetAxis("Mouse Y"));
+        Vector2 _lookInput = _inputActions.Gameplay.Look.ReadValue<Vector2>();
+        
+        _udInput = _lookInput.y;
+        // _ewInput = _lookInput.x;
+        
+        pitch += speedV * - _udInput;
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
 
         Quaternion rotation = Quaternion.Euler(pitch, 0, 0);
